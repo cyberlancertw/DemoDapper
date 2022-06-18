d8f875d95c35b9ab0967a114abf5e790068ce3c2
cyberlancer <cyberlancer@gmail.com>
2022-06-18 16:42:09 +0800

    typo

--- DemoDapper/Program.cs
+++ DemoDapper/Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<DapperContext>();
-//builder.Services.AddScoped<ProductFactory, ProductFactory>();
 builder.Services.AddScoped<IFactory<Product>, ProductFactory>();
 builder.Services.AddScoped<IFactory<Order>, OrderFactory>();
 builder.Services.AddControllersWithViews();
@@ -30,21 +29,3 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
-
-
-
-// builder.Services.AddScoped<ProductFactory, ProductFactory>();
-// 則 Controller 用
-// private readonly ProductFactory _factory;
-// public ProductController(ProductFactory factory)
-// {
-//     _factory = factory;
-// }
-
-// builder.Services.AddScoped<IFactory<Product>, ProductFactory>();
-// 則 Controller 用
-// private readonly ProductFactory _factory;
-// public ProductController(IFactory<Product> factory)
-// {
-//     _factory = factory as ProductFactory;
-// }
